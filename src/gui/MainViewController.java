9c95976f0a7790cea6794bfff421ce2da51b1b44
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-09 21:51:11 -0300

    Update MainViewController.java

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -33,19 +34,22 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->{
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x-> {});
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
 
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 
 		try {
 			// CRIA A TELA
@@ -70,41 +74,13 @@ public class MainViewController implements Initializable {
 			// AGORA PRECIAMOS REUNIR ELES.
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
-
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-
-	private synchronized void loadView2(String absoluteName) {
-
-		try {
-			// CRIA A TELA
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			// ATRIBUI A TELA A UMA VARIAVEL VBOX
-			VBox newVBox = loader.load();
-			// CHAMA A CENA PRINCIPAL
-
-			Scene mainScene = Main.getMainScene();
-			// ATRIBUI O CONTEUDO VBOX DA CENA PRINCIPAL A UMA VARIAVEL
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-			// SALVAMOS O CONTEUDO EM UMA VARIAVEL, DPS LIMPAMOS O CONTEUDO:
-
-			// CHAMA A VARIAVEL COM O CONTEUDO DA CENA PRINCIPAL E PUXA O FILHO NA POSIÇÃO 0
-			// SENDO O PRIMEIRO
-			// Q É O PRIMEIRO VBOX DA JANELA PRINCIAPL
-			Node mainMenu = mainVBox.getChildren().get(0);
-			// LIMPA TODO O CONTEUDO DOS FILHOS DA CENA PRINCIPAL
-			mainVBox.getChildren().clear();
-
-			// AGORA PRECIAMOS REUNIR ELES.
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
+			// RECEBE O TIPO T VINDO DO CONSUMER, NO CASO DEPARTMENTLIST CONTROLLER
+			// VINDO DA FUNÇÃO onMenuDepartmentAction
+			T controller = loader.getController();
+			// AGORA CHAMAMOS A CLASSE Q INICIA ESSE COMANDO VINDO DO CONSUMER
+			initializingAction.accept(controller);
 			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			
 
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
