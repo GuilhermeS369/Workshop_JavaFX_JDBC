103813ae7ae58f7efd86a1df36c189228c179098
Guilherme Santos <guilherme460@yahoo.com.br>
2022-05-10 14:15:21 -0300

    DepartmentForm Design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -31,13 +41,17 @@ public class DepartmentListController implements Initializable {
 	private TableColumn<Department, String> tableColumnName;
 	
 	@FXML
+	private TextField nameField;
+	
+	@FXML
 	private Button btNew;
 	
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage );
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -64,5 +78,34 @@ public class DepartmentListController implements Initializable {
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			// CRIA A TELA
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			// INSTANCIAMOS UM NOVO STAGE PARA POR UM NA FRENTE DO OUTRO:
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department ata");
+			// CRIAMOS UMA NOVA CENA DIZENDO O ELEMENTO RAIZ DELA COMO PANE
+			dialogStage.setScene(new Scene(pane));
+			// PROIBIDO REDIMENSIOANR TELA:
+			dialogStage.setResizable(false);
+			// PAI DESSA JANELA
+			dialogStage.initOwner(parentStage);
+			// METODO Q DIZ SE É MODAL OU NAO(EM QNT N FECHAR, N PODE ACESSAR A JANELA ATRAS)
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			// CHAMA A TELA
+			dialogStage.showAndWait();
+			
+			
+						
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	
 	}
 }
